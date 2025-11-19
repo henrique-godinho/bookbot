90a746ee4d9c87d29edae0533f5030318c894c37
henrique-godinho <henrique.f.godinho@gmail.com>
2025-11-19 09:59:45 +0000

    update

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        report(word_count(file_contents), char_count(file_contents))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
 
-def word_count(text):
-    words = text.split()
-    return len(words)
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
-def char_count(text):
-    lowered_text = text.lower()
-    response = {}
-    for char in lowered_text:
-        if char.isalpha():
-            if char not in response:
-                response[char] = 1
-            else:
-                response[char] += 1
-    return response
 
-def report(word_count, chars):
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{word_count} words found in the document")
-    print("")
-    print("")
-    for key, value in chars.items():
-        print(f"The '{key}' character was found {value} times")
-    print("--- End report ---")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
